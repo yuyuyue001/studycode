302aa3a170b3712fc2d7d5d604440b534f8969b9
yuyuyue001 <yuyuyue001@sina.com>
2024-07-07 19:45:57 +0800

    Add files via upload

---
+++ s1.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  7 19:17:22 2024
+
+@author: Lenovo
+"""
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+print(bicycles[0])
+
+print(bicycles[0].title())
+
+print(bicycles[1])
+print(bicycles[3])
+
+print(bicycles[-1])
+
+message = "My first bicycle was a " + bicycles[0].title() + "."
+print(message)
